404d1813fd84b1bfc13731add88e4d521e4eb20d
Filip Hlavka <fhlavka@sygic.com>
2022-05-10 15:39:04 +0200

    style: CustomPlacesManager

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/places/CustomPlacesTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/places/CustomPlacesTests.kt
@@ -9,21 +9,21 @@ import org.junit.Test
 
 class CustomPlacesTests : BaseTest() {
 
-    lateinit var customPlacesmanager: CustomPlacesManager
+    lateinit var customPlacesManager: CustomPlacesManager
 
     override fun setUp() {
         super.setUp()
-        customPlacesmanager = CustomPlacesManagerProvider.getInstance().get()
+        customPlacesManager = CustomPlacesManagerProvider.getInstance().get()
     }
 
     @Test
     fun downloadCustomPlacesSlovakia() {
         val uninstallResultListener: CustomPlacesManager.InstallResultListener = mock(verboseLogging = true)
-        customPlacesmanager.uninstallOfflinePlaces("sk", uninstallResultListener)
+        customPlacesManager.uninstallOfflinePlaces("sk", uninstallResultListener)
         verify(uninstallResultListener, timeout(2_000L)).onResult(any())
         val resultListener : CustomPlacesManager.InstallResultListener = mock(verboseLogging = true)
         val progressListener : CustomPlacesManager.InstallProgressListener = mock(verboseLogging = true)
-        customPlacesmanager.installOfflinePlaces("sk", resultListener, progressListener)
+        customPlacesManager.installOfflinePlaces("sk", resultListener, progressListener)
         verify(progressListener, timeout(5_000L)).onProgress(any())
         verify(resultListener, timeout(5_000L)).onResult(eq(CustomPlacesManager.InstallResult.SUCCESS))
         verify(resultListener, never()).onResult(eq(CustomPlacesManager.InstallResult.FAIL))
@@ -31,9 +31,9 @@ class CustomPlacesTests : BaseTest() {
 
     @Test
     fun setModeGetModeTest() {
-        customPlacesmanager.setMode(CustomPlacesManager.Mode.ONLINE)
-        Assert.assertEquals(customPlacesmanager.getMode(), CustomPlacesManager.Mode.ONLINE)
-        customPlacesmanager.setMode(CustomPlacesManager.Mode.OFFLINE)
-        Assert.assertEquals(customPlacesmanager.getMode(), CustomPlacesManager.Mode.OFFLINE)
+        customPlacesManager.setMode(CustomPlacesManager.Mode.ONLINE)
+        Assert.assertEquals(customPlacesManager.getMode(), CustomPlacesManager.Mode.ONLINE)
+        customPlacesManager.setMode(CustomPlacesManager.Mode.OFFLINE)
+        Assert.assertEquals(customPlacesManager.getMode(), CustomPlacesManager.Mode.OFFLINE)
     }
 }
