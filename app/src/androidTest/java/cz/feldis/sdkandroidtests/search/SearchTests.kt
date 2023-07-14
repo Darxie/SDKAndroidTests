edf338e8b9b5ea24cc64afd4c698257388bbc3f6
Filip Hlavka <fhlavka@sygic.com>
2023-07-14 16:23:43 +0200

    Re-worked two search tests

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/search/SearchTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/search/SearchTests.kt
@@ -214,94 +214,52 @@ class SearchTests : BaseTest() {
             }, any())
     }
 
-    /**
-     * Search places and loadExternalPlaceId test
-     *
-     * In this test we create place request with Radius 1000, GeoCoordinates (Egypt 29.978296, 31.132839)
-     * and with category ImportantTouristAttraction. We verify that search places with this request return onPlaceLoaded
-     * and list of objects  is not empty, also whether places link name in list is equals "Pyramid of Khufu"
-     * and "El Giza Monuments Area". According to this links we can  verify whether loadExternalPlaceId with place link from previous search places
-     * equals expected Khufu External Id and expected Giza External Id
-     */
-
     @Test
     fun loadExternalPlaceId() {
         mapDownloadHelper.installAndLoadMap("eg")
-        val searchManager = SearchManagerProvider.getInstance().get()
         val placesManager = PlacesManagerProvider.getInstance().get()
-        val listener = Mockito.mock(
-            PlacesListener::class.java,
-            Mockito.withSettings().verboseLogging()
-        )
 
-        val listenerExternalIdKhufu = Mockito.mock(
-            PlacesManager.PlaceExternalIdListener::class.java,
-            Mockito.withSettings().verboseLogging()
-        )
-
-        val listenerExternalIdGiza = Mockito.mock(
-            PlacesManager.PlaceExternalIdListener::class.java,
-            Mockito.withSettings().verboseLogging()
-        )
+        val listenerExternalIdKhufu : PlacesManager.PlaceExternalIdListener = mock(verboseLogging = true)
+        val listenerExternalIdGiza : PlacesManager.PlaceExternalIdListener = mock(verboseLogging = true)
 
         val category = listOf(PlaceCategories.ImportantTouristAttraction)
-        val request = PlaceRequest(GeoCoordinates(29.978296, 31.132839), category, 1000)
-        val session = searchManager.newOfflineSession()
-
-        session.searchPlaces(request, listener)
+        val request = PlaceRequest(GeoCoordinates(29.978296, 31.132839), category, 500)
 
         val expectedKhufuExternalId = "002f80f0-002d-bef0-6632-356165633562"
         val expectedGizaExternalId = "002f80f0-002d-bfb8-3665-383732623732"
 
-        verify(listener, timeout(10000))
-            .onPlacesLoaded(argThat {
-                if (this.isEmpty()) {
-                    Timber.e("The list returned by searchPlaces is empty")
-                    return@argThat false
-                }
-
-                for (place in this) {
-                    if (place.link.name == "Pyramid of Khufu") {
-                        placesManager.loadExternalPlaceId(place.link, listenerExternalIdKhufu)
-                        verify<PlacesManager.PlaceExternalIdListener>(
-                            listenerExternalIdKhufu,
-                            Mockito.timeout(5000)
-                        )
-                            .onExternalPlaceIdLoaded(argThat {
-                                if (searchHelper.byteArrayToUUID(this) != expectedKhufuExternalId) {
-                                    return@argThat false
-                                }
-                                true
-                            })
-                    }
-                    if (place.link.name == "El Giza Monuments Area") {
-                        placesManager.loadExternalPlaceId(place.link, listenerExternalIdGiza)
-                        verify<PlacesManager.PlaceExternalIdListener>(
-                            listenerExternalIdGiza,
-                            Mockito.timeout(5000)
-                        )
-                            .onExternalPlaceIdLoaded(argThat {
-                                if (searchHelper.byteArrayToUUID(this) != expectedGizaExternalId) {
-                                    return@argThat false
-                                }
-                                true
-                            })
-                    }
-                }
-                true
-            }, any())
+        val placesList = searchHelper.offlineSearchPlaces(request)
+        for (place in placesList) {
+            if (place.link.name == "Pyramid of Khufu") {
+                placesManager.loadExternalPlaceId(place.link, listenerExternalIdKhufu)
+                verify(
+                    listenerExternalIdKhufu,
+                    Mockito.timeout(5000)
+                )
+                    .onExternalPlaceIdLoaded(argThat {
+                        if (searchHelper.byteArrayToUUID(this) != expectedKhufuExternalId) {
+                            return@argThat false
+                        }
+                        true
+                    })
+            }
+            if (place.link.name == "El Giza Monuments Area") {
+                placesManager.loadExternalPlaceId(place.link, listenerExternalIdGiza)
+                verify(
+                    listenerExternalIdGiza,
+                    Mockito.timeout(5000)
+                )
+                    .onExternalPlaceIdLoaded(argThat {
+                        if (searchHelper.byteArrayToUUID(this) != expectedGizaExternalId) {
+                            return@argThat false
+                        }
+                        true
+                    })
+            }
+        }
 
     }
 
-    /**
-     * Search places test with valid string category
-     *
-     *
-     * In this test we create place request with Radius 100, GeoCoordinates (Egypt 29.978296, 31.132839)
-     * and with category ImportantTouristAttraction. We verify that search places with this request return onPlaceLoaded
-     * and list of objects  is not empty, also whether places link name and details is not empty. We also expected that
-     * details of place with link name Pyramid of Khufu will be same as expectedDetail.
-     */
     @Test
     fun searchPlacesDetails() {
         mapDownloadHelper.installAndLoadMap("eg")
@@ -312,39 +270,21 @@ class SearchTests : BaseTest() {
         val request = PlaceRequest(GeoCoordinates(29.9774, 31.1323), categories, 1000)
         val session = searchManager.newOfflineSession()
 
-        val expectedDetail = mutableListOf<PlaceDetail>()
-        expectedDetail.add(PlaceDetail(PlaceDetailAttributes.City, "Desert of Giza Governorate"))
-//        expectedDetail.add(PlaceDetail(PlaceDetailAttributes.Street, "al-Qahirah - al-Fayyoum"))
-        expectedDetail.add(PlaceDetail(PlaceDetailAttributes.Iso, "eg"))
-        expectedDetail.add(PlaceDetail(PlaceDetailAttributes.Entry, "29.97429;31.12732"))
-
         session.searchPlaces(request, listener)
 
-        verify(listener, Mockito.timeout(10_000L))
-            .onPlacesLoaded(argThat {
-                Timber.d("Size of searchPlaces response: ${this.size}")
+        val captor = argumentCaptor<List<Place>>()
 
-                if (this.isEmpty()) {
-                    Timber.e("The list returned by searchPlaces is empty")
-                    return@argThat false
-                }
-                if (this.size < 2) {
-                    Timber.e("The list returned by searchPlaces has less than 2 objects")
-                    return@argThat false
-                }
-                for (place in this) {
-                    if (place.link.name.isEmpty() || place.details.isEmpty()) {
-                        Timber.e("Place link name is empty or place details are empty ")
-                        return@argThat false
-                    }
-                    if (place.link.name == "Pyramid of Khufu")
-                        if (place.details != expectedDetail) {
-                            Timber.e("Place details not equal to expected details ")
-                            return@argThat false
-                        }
-                }
-                true
-            }, any())
+        verify(listener, Mockito.timeout(10_000L))
+            .onPlacesLoaded(captor.capture(), any())
+
+        val placesList = captor.allValues.flatten()
+        assert(placesList.isNotEmpty())
+        assert(placesList.size > 2)
+        for (place in placesList) {
+            if (place.link.name == "Pyramid of Khufu")
+                assert(place.details[0].value == "Desert of Giza Governorate")
+            assert(place.details.isNotEmpty())
+        }
     }
 
     @Test
