f565ec4bf09bdfad648804ed6724abad0b7605f0
Filip Hlavka <fhlavka@sygic.com>
2024-04-12 16:20:57 +0200

    Add max trailers test

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/navigation/VehicleAidTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/navigation/VehicleAidTests.kt
@@ -19,25 +19,19 @@ import com.sygic.sdk.navigation.NavigationManager
 import com.sygic.sdk.navigation.NavigationManagerProvider
 import com.sygic.sdk.navigation.routeeventnotifications.RestrictionInfo
 import com.sygic.sdk.position.GeoCoordinates
-import com.sygic.sdk.position.GeoPosition
 import com.sygic.sdk.route.RoutingOptions
 import com.sygic.sdk.route.RoutingOptions.NearestAccessiblePointStrategy
 import com.sygic.sdk.route.RoutingOptions.RoutingService
 import com.sygic.sdk.route.RoutingOptions.TransportMode
 import com.sygic.sdk.route.RoutingOptions.VehicleRestrictions
-import com.sygic.sdk.route.simulator.NmeaLogSimulatorProvider
-import com.sygic.sdk.route.simulator.PositionSimulator
-import com.sygic.sdk.route.simulator.PositionSimulator.PositionSimulatorListener
 import cz.feldis.sdkandroidtests.BaseTest
 import cz.feldis.sdkandroidtests.SygicActivity
 import cz.feldis.sdkandroidtests.TestMapFragment
 import cz.feldis.sdkandroidtests.mapInstaller.MapDownloadHelper
 import cz.feldis.sdkandroidtests.routing.RouteComputeHelper
-import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
-import timber.log.Timber
 
 class VehicleAidTests: BaseTest() {
     private lateinit var routeCompute: RouteComputeHelper
@@ -145,6 +139,55 @@ class VehicleAidTests: BaseTest() {
                 if (vehicleAidInfo.restriction.type == RestrictionInfo.RestrictionType.DimensionalHeightMax) {
                     if (vehicleAidInfo.restrictedRoad)
                         return@argThat true
+                }
+            }
+            return@argThat false
+        })
+
+        NavigationManagerProvider.getInstance().get().removeOnVehicleAidListener(listener)
+        NavigationManagerProvider.getInstance().get().stopNavigation()
+        scenario.moveToState(Lifecycle.State.DESTROYED)
+    }
+
+    @Test
+    @Ignore("Only works with HERE Maps")
+    fun vehicleAidMaxTrailers() {
+        val mapFragment = TestMapFragment.newInstance(getInitialCameraState())
+        // create test scenario with activity & map fragment
+        val scenario = ActivityScenario.launch(SygicActivity::class.java).onActivity {
+            it.supportFragmentManager
+                .beginTransaction()
+                .add(android.R.id.content, mapFragment)
+                .commitNow()
+        }
+
+        val mapView = getMapView(mapFragment)
+        val logisticInfoSettings = LogisticInfoSettings()
+        logisticInfoSettings.trailersCount = 5
+        logisticInfoSettings.vehicleType = LogisticInfoSettings.VehicleType.Truck
+        mapView.setLogisticInfoSettings(logisticInfoSettings)
+        mapView.cameraModel.position = GeoCoordinates( 57.6456, 14.9804)
+
+        val listener = mock<NavigationManager.OnVehicleAidListener>(verboseLogging = true)
+
+        val route = routeCompute.offlineRouteCompute(
+            start = GeoCoordinates(57.6456, 14.9804),
+            destination = GeoCoordinates( 57.6452, 14.9781),
+            routingOptions = RoutingOptions().apply {
+                transportMode = TransportMode.TransportTruck
+                routingService = RoutingService.Offline
+                setUseEndpointProtection(true)
+                napStrategy = NearestAccessiblePointStrategy.Disabled
+            }
+        )
+
+        NavigationManagerProvider.getInstance().get().setRouteForNavigation(route)
+        NavigationManagerProvider.getInstance().get().addOnVehicleAidListener(listener)
+
+        verify(listener, timeout(10_000)).onVehicleAidInfo(argThat {
+            for (vehicleAidInfo in this) {
+                if (vehicleAidInfo.restriction.type == RestrictionInfo.RestrictionType.LimitsMaxTrailers) {
+                    return@argThat true
                 }
             }
             return@argThat false
