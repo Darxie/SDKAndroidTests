a1118bae033f57063d0f01a2535221e5e73adcaf
Filip Hlavka <fhlavka@sygic.com>
2024-06-11 13:14:30 +0200

    Re-written position manager tests

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/position/PositionManagerTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/position/PositionManagerTests.kt
@@ -1,13 +1,16 @@
 package cz.feldis.sdkandroidtests.position
 
-import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.timeout
 import com.nhaarman.mockitokotlin2.verify
+import com.sygic.sdk.position.CustomPositionUpdater
+import com.sygic.sdk.position.GeoCoordinates
+import com.sygic.sdk.position.GeoPosition
 import com.sygic.sdk.position.PositionManager
 import com.sygic.sdk.position.PositionManagerProvider
 import cz.feldis.sdkandroidtests.BaseTest
-import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 
 class PositionManagerTests: BaseTest() {
@@ -16,8 +19,33 @@ class PositionManagerTests: BaseTest() {
     fun getLastValidLocationTest() {
         val positionListener : PositionManager.OnLastKnownPositionListener = mock(verboseLogging = true)
 
-        assertNotNull(PositionManagerProvider.getInstance().get().lastKnownPosition)
         PositionManagerProvider.getInstance().get().getLastKnownPosition(positionListener)
-        verify(positionListener, timeout(5_000L)).onLastKnownPosition(any())
+        verify(positionListener, timeout(5_000L)).onLastKnownPosition(argThat {
+            if (this.isValid) {
+                return@argThat true
+            }
+            false
+        })
+    }
+
+    @Test
+    fun customPositionUpdaterTest() {
+        val mPositionManager = PositionManagerProvider.getInstance().get()
+
+        val positionChangeListener : PositionManager.PositionChangeListener = mock(verboseLogging = true)
+        val operationListener : PositionManager.OnOperationComplete = mock(verboseLogging = true)
+        val updatePositionListener : CustomPositionUpdater.OnOperationComplete = mock(verboseLogging = true)
+
+        val customPositionUpdater = CustomPositionUpdater()
+        mPositionManager.setCustomPositionUpdater(customPositionUpdater, operationListener)
+        mPositionManager.addPositionChangeListener(positionChangeListener)
+        verify(operationListener, timeout(5_000L)).onComplete()
+
+        val geoCoordinates = GeoCoordinates(48.11111,17.55555)
+        val geoPosition = GeoPosition(geoCoordinates, 69.0, 42.0F, 5000L)
+        customPositionUpdater.updatePosition(geoPosition, updatePositionListener)
+        verify(updatePositionListener, timeout(5_000L)).onComplete()
+
+        verify(positionChangeListener, timeout(10_000L)).onPositionChanged(eq(geoPosition))
     }
 }
