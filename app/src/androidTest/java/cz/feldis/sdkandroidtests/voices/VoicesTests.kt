3850ba5d50777cc1fc3631d576d7d40f011d9126
Filip Hlavka <fhlavka@sygic.com>
2025-01-13 16:25:54 +0100

    add getVoiceStatusTest

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/voices/VoicesTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/voices/VoicesTests.kt
@@ -1,16 +1,19 @@
 package cz.feldis.sdkandroidtests.voices
 
-import org.mockito.kotlin.*
 import com.sygic.sdk.OperationStatus
+import com.sygic.sdk.voice.VoiceDownload
+import com.sygic.sdk.voice.VoiceDownloadProvider
 import com.sygic.sdk.voice.VoiceEntry
 import com.sygic.sdk.voice.VoiceManager
 import com.sygic.sdk.voice.VoiceManager.InstalledVoicesCallback
 import com.sygic.sdk.voice.VoiceManager.OnSetVoiceCallback
 import com.sygic.sdk.voice.VoiceManagerProvider
 import cz.feldis.sdkandroidtests.BaseTest
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Test
+import org.mockito.kotlin.*
 
 class VoicesTests : BaseTest() {
 
@@ -59,5 +62,28 @@ class VoicesTests : BaseTest() {
                 voicesManager.setVoice(voice)
             }
         verify(onSetVoiceCallback, timeout(10_000L)).onSetVoice("en-au-x-aub-local", true)
+    }
+
+    @Test
+    fun getVoiceStatusTest() = runBlocking {
+        val listener: VoiceDownload.AvailableVoicesCallback = mock(verboseLogging = true)
+        val statusCallback: VoiceEntry.OnGetStatusCallback = mock(verboseLogging = true)
+        VoiceDownloadProvider.getInstance().get().getAvailableVoiceList(listener)
+
+        val captor = argumentCaptor<List<VoiceEntry>>()
+        verify(listener, timeout(10_000L)).onAvailableVoiceList(
+            captor.capture(),
+            eq(OperationStatus(OperationStatus.Result.Success, ""))
+        )
+
+        val voices = captor.firstValue
+        assertFalse(voices.isEmpty())
+
+        voices.forEach { voiceEntry ->
+            voiceEntry.getStatus(statusCallback)
+        }
+
+        // Verify the exact number of invocations
+        verify(statusCallback, timeout(10_000L).times(voices.size)).onStatus(any())
     }
 }
