d256d94a604d3c049d94fcedee589c0fabadcc74
Filip Hlavka <filip.hlavka@gmail.com>
2025-05-08 11:00:37 +0200

    BC and new tests

--- app/src/androidTest/java/cz/feldis/sdkandroidtests/routing/RouteComputeTests.kt
+++ app/src/androidTest/java/cz/feldis/sdkandroidtests/routing/RouteComputeTests.kt
@@ -666,6 +666,43 @@ class RouteComputeTests : BaseTest() {
         )
     }
 
+    @Test
+    fun transitCountriesPreserveOrderTest_ThroughLiechtensteinToHungary() {
+        mapDownloadHelper.installAndLoadMap("ch") // Switzerland
+        mapDownloadHelper.installAndLoadMap("li") // Liechtenstein
+        mapDownloadHelper.installAndLoadMap("at") // Austria
+        mapDownloadHelper.installAndLoadMap("sk") // Slovakia
+        mapDownloadHelper.installAndLoadMap("hu") // Hungary
+
+        // Define route: start in Switzerland, end in Hungary
+        val start = GeoCoordinates(47.3769, 8.5417) // Zurich
+        val waypoint1 = GeoCoordinates(47.1416, 9.5215) // Liechtenstein
+        val waypoint2 = GeoCoordinates(47.5162, 14.5501) // Austria
+        val waypoint3 = GeoCoordinates(48.1486, 17.1077) // Slovakia
+        val destination = GeoCoordinates(47.4979, 19.0402) // Budapest
+
+        val route = RouteComputeHelper().offlineRouteCompute(
+            start,
+            destination,
+            waypoints = listOf(waypoint1, waypoint2, waypoint3)
+        )
+
+        // Expected order of transition countries
+        val expectedCountries = listOf(
+            TransitCountryInfo("ch", emptyList()),
+            TransitCountryInfo("li", emptyList()),
+            TransitCountryInfo("at", emptyList()),
+            TransitCountryInfo("sk", emptyList()),
+            TransitCountryInfo("hu", emptyList())
+        )
+
+        // Mock and verify the transit countries info listener
+        val listener: TransitCountriesInfoListener = mock(verboseLogging = true)
+        route.getTransitCountriesInfo(listener)
+
+        verify(listener, timeout(5_000L)).onTransitCountriesInfo(expectedCountries)
+    }
+
     /**
      * https://jira.sygic.com/browse/SDC-13368
      *
@@ -696,9 +733,42 @@ class RouteComputeTests : BaseTest() {
             route.routeRequest.routingOptions.routeAvoids.countryRouteAvoidables
 
         listOf(startCountry, destinationCountry).forEach { country ->
-            countryRouteAvoidables[country]?.let { avoidSet ->
-                avoidSet.forEach { avoidType ->
-                    assert(avoidType != RouteAvoids.Type.Country) { "Country avoidable type found in $country" }
+            countryRouteAvoidables.find { it.first == country }?.second?.forEach { avoidType ->
+                assert(avoidType != RouteAvoids.Type.Country) { "Country avoidable type found in $country" }
+            }
+        }
+    }
+
+    @Test
+    fun avoidableCountryTest_startAndEndCountryMustNotBeAvoided() {
+        mapDownloadHelper.installAndLoadMap("ch") // Switzerland
+        mapDownloadHelper.installAndLoadMap("li") // Liechtenstein
+        mapDownloadHelper.installAndLoadMap("at") // Austria
+        mapDownloadHelper.installAndLoadMap("sk") // Slovakia
+        mapDownloadHelper.installAndLoadMap("hu") // Hungary
+
+        val start = GeoCoordinates(47.3769, 8.5417) // Zurich
+        val waypoint1 = GeoCoordinates(47.1416, 9.5215) // Liechtenstein
+        val waypoint2 = GeoCoordinates(47.5162, 14.5501) // Austria
+        val waypoint3 = GeoCoordinates(48.1486, 17.1077) // Slovakia
+        val destination = GeoCoordinates(47.4979, 19.0402) // Budapest
+
+        val route = RouteComputeHelper().offlineRouteCompute(
+            start,
+            destination,
+            waypoints = listOf(waypoint1, waypoint2, waypoint3)
+        )
+
+        val startCountry = "ch"
+        val destinationCountry = "hu"
+
+        val countryRouteAvoidables: List<Pair<String, Set<RouteAvoids.Type>>> =
+            route.routeRequest.routingOptions.routeAvoids.countryRouteAvoidables
+
+        listOf(startCountry, destinationCountry).forEach { country ->
+            countryRouteAvoidables.find { it.first == country }?.second?.forEach { avoidType ->
+                assert(avoidType != RouteAvoids.Type.Country) {
+                    "Country avoidable type should not be applied to $country"
                 }
             }
         }
